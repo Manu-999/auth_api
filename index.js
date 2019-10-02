1e6c0bc0125c8341e258b932e9d0b002cd403be5
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-02 17:41:47 +0200

    mongoose installed and connected to db

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+// Connect to DB
+
+mongoose.connect('mongodb://127.0.0.1:27017', () =>
+  console.log('Connected to DB')
+);
 
 // Import Routes
 
