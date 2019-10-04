72a23f5772119e6c7634a32afa6918ba23818dde
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-04 19:56:16 +0200

    added validation library and validation error handling

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,28 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
+// VALIDATION
+
+const Joi = require('@hapi/joi');
+
+const schema = {
+  name: Joi.string()
+    .min(6)
+    .required(),
+  email: Joi.string()
+    .min(6)
+    .required()
+    .email(),
+  password: Joi.string()
+    .min(6)
+    .required()
+};
+
 router.post('/register', async (req, res) => {
+  // VALIDATING DATA BEOFRE
+
+  Joi.validate(req.body, schema);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
