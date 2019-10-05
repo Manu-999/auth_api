741fdd82e1b5585bd45379d346c66aaf0667defd
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-05 12:42:56 +0200

    hashed passwords feature added and working

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const { registerValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
 router.post('/register', async (req, res) => {
   // VALIDATING DATA BEOFRE
@@ -12,17 +13,22 @@ router.post('/register', async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist) return res.status(400).send('Email already registered');
 
+  // Hash the password
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
   // Creating a new User
 
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   });
 
   try {
     const savedUser = await user.save();
-    res.send(savedUser);
+    res.send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
