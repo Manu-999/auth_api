b28b4e43529ae7bc28e8019b40ce66bcbfaab692
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-05 12:52:25 +0200

    login request working

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const User = require('../models/User');
-const { registerValidation } = require('../validation');
+const { registerValidation, loginValidation } = require('../validation');
 const bcrypt = require('bcryptjs');
+
+// Register
 
 router.post('/register', async (req, res) => {
   // VALIDATING DATA BEOFRE
@@ -32,6 +34,26 @@ router.post('/register', async (req, res) => {
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+// Login
+
+router.post('/login', async (req, res) => {
+  // VALIDATING DATA BEOFRE
+  const { error } = loginValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // Checking if the user exists
+
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.status(400).send('The email is not found.');
+
+  // Checking the password
+
+  const validPassword = await bcrypt.compare(req.body.password, user.password);
+  if (!validPassword) return res.status(400).send('Invalid password.');
+
+  res.send('Logged in');
 });
 
 module.exports = router;
