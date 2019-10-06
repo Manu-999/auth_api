81cf28ec8d4f2aeecfd4a51221686a0aaf8e94a3
Manuel Balbás <manuelbalbas@Manuels-MacBook-Pro.local>
2019-10-06 12:16:07 +0200

    created auth tokens with JWT and private route

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+// Import routes
+
+const authRoute = require('./routes/auth');
+const postRoute = require('./routes/posts');
+
 dotenv.config();
 
 // Connect to DB
@@ -15,12 +20,9 @@ mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () =>
 
 app.use(express.json());
 
-// Import Routes
-
-const authRoute = require('./routes/auth');
-
 // Routes Middlewares
 
 app.use('/api/user', authRoute);
+app.use('/api/posts', postRoute);
 
 app.listen(3000, console.log('Server up and running'));
